cad738e14fc85f08510b7a647f07ccd4e0960356
Adarsh Tripathi <iec2019024@iiita.ac.in>
2020-06-19 20:03:27 +0530

    added packages for making files run on local server

---
+++ app.js
@@ -0,0 +1,26 @@
+var bodyParser = require("body-parser");
+var expressSanitizer = require("express-sanitizer");
+var methodOverride = require("method-override");
+var mongoose = require("mongoose");
+var express = require("express");
+var app = express();
+
+//App Configuration
+// ======== //
+mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true, useUnifiedTopology: true });
+app.set("view engine","ejs");
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+// ======== //
+
+// ROUTES
+app.get("/",function(req,res){
+    res.render("index");
+});
+
+//Listening to localhost
+app.listen(1000, function(){
+    console.log("Server is running");
+})
